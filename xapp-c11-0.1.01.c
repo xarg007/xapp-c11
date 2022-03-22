013998995c73dc410bf52d2c61a3ffbf775b4c8c
xarg007 <gaolz@139.com>
2022-03-22 18:57:35 +0800

    test 003

---
+++ xapp-c11-0.1.01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("  >> the app starting ... ...\n");
+    
+    do
+    {
+        /* code */
+        printf("   >>> do something ... ...\n");
+    } while (0);
+    
+    printf("  >> the app exit.\n");
+
+    return 0;
+}
